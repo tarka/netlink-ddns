95a3babb30faf344d561d906c3f6531c57b13508
Steve <tarkasteve@gmail.com>
2025-07-26 11:44:50 +1000

    Interim checkin; move rtnetlink logic into own function.

--- src/systemd/mod.rs
+++ src/systemd/mod.rs
@@ -1,6 +1,61 @@
 
+use std::{future, net::{IpAddr, Ipv4Addr}};
+
 use anyhow::{bail, Result};
+use futures::{stream, TryStreamExt};
+use rtnetlink::packet_route::{address::AddressAttribute, AddressFamily};
 use tracing::{error, info, warn};
+
+async fn get_if_addr(ifname: &String) -> Result<Ipv4Addr> {
+    let (connection, handle, _) = rtnetlink::new_connection()?;
+    tokio::spawn(connection);
+
+    let mut links = handle.link().get()
+        .match_name("wlp10s0".to_string())
+        .execute();
+
+    while let Some(link) = links.try_next().await? {
+        // Fetch link addresses
+        let addrs = handle.address().get()
+            .set_link_index_filter(link.header.index)
+            .execute()
+            // Extract attributes
+            .try_filter_map(|a| future::ready(
+                if a.header.family == AddressFamily::Inet {
+                    Ok(Some(a.attributes))
+                } else {
+                    Ok(None)
+                })
+            )
+            .map_ok(|attrs| stream::iter(
+                attrs.into_iter()
+                    .map(|a| Ok::<AddressAttribute, rtnetlink::Error>(a))
+            ))
+            .try_flatten()
+            .try_collect::<Vec<AddressAttribute>>().await?
+            // Extract relevant addresses
+            .into_iter()
+            .flat_map(|a| if let AddressAttribute::Address(addr) = a {
+                Some(addr)
+            } else {
+                None
+            })
+            .collect::<Vec<IpAddr>>();
+
+        if addrs.len() == 0 {
+            bail!("No IPv4 address found for interface {ifname}")
+        }
+        if addrs.len() > 1 {
+            // Probably not possible under Linux? Check anyway.
+            bail!("Multiple IPv4 addresses found on for interface {ifname}")
+        }
+        for addr in addrs {
+            println!("Attr: {addr:#?}");
+        }
+    }
+
+    Ok(Ipv4Addr::new(1,1,1,1))
+}
 
 #[cfg(test)]
 mod tests {
@@ -14,46 +69,6 @@ mod tests {
     #[tokio::test]
     #[traced_test]
     async fn test_fetch_addrs() -> Result<()> {
-        let (connection, handle, _) = new_connection()?;
-        tokio::spawn(connection);
-
-        let mut links = handle.link().get()
-            .match_name("wlp10s0".to_string())
-            .execute();
-
-        while let Some(link) = links.try_next().await? {
-            // Fetch link addresses
-            let addrs = handle.address().get()
-                .set_link_index_filter(link.header.index)
-                .execute()
-                // Extract attributes
-                .try_filter_map(|a| future::ready(
-                    if a.header.family == AddressFamily::Inet {
-                        Ok(Some(a.attributes))
-                    } else {
-                        Ok(None)
-                    })
-                )
-                .map_ok(|attrs| stream::iter(
-                    attrs.into_iter()
-                        .map(|a| Ok::<AddressAttribute, rtnetlink::Error>(a))
-                ))
-                .try_flatten()
-                .try_collect::<Vec<AddressAttribute>>().await?
-                // Extract relevant addresses
-                .into_iter()
-                .flat_map(|a| if let AddressAttribute::Address(addr) = a {
-                    Some(addr)
-                } else {
-                    None
-                })
-                .collect::<Vec<IpAddr>>();
-
-            println!("LEN = {}", addrs.len());
-            for addr in addrs {
-                println!("Attr: {addr:#?}");
-            }
-        }
 
         assert!(false);
         Ok(())
