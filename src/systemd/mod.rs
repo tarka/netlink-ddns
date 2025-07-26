2e0be20ec3e9f75cfe144c3d874b1293dcb14acb
Steve <tarkasteve@gmail.com>
2025-07-26 13:02:57 +1000

    More address fetch cleanup.

--- src/systemd/mod.rs
+++ src/systemd/mod.rs
@@ -1,8 +1,8 @@
 
 use std::{future, net::{IpAddr, Ipv4Addr}};
 
-use anyhow::{bail, Result};
-use futures::{stream, TryStreamExt};
+use anyhow::{bail, Context, Result};
+use futures::{stream, StreamExt, TryStreamExt};
 use rtnetlink::packet_route::{address::AddressAttribute, AddressFamily};
 use tracing::{error, info, warn};
 
@@ -10,67 +10,74 @@ async fn get_if_addr(ifname: &String) -> Result<Ipv4Addr> {
     let (connection, handle, _) = rtnetlink::new_connection()?;
     tokio::spawn(connection);
 
-    let mut links = handle.link().get()
-        .match_name("wlp10s0".to_string())
-        .execute();
+    let link = handle.link().get()
+        .match_name(ifname.clone())
+        .execute()
+        .try_next().await?
+        .context("Failed to find interface {ifname}")?;
 
-    while let Some(link) = links.try_next().await? {
-        // Fetch link addresses
-        let addrs = handle.address().get()
-            .set_link_index_filter(link.header.index)
-            .execute()
-            // Extract attributes
-            .try_filter_map(|a| future::ready(
-                if a.header.family == AddressFamily::Inet {
-                    Ok(Some(a.attributes))
-                } else {
-                    Ok(None)
-                })
-            )
-            .map_ok(|attrs| stream::iter(
-                attrs.into_iter()
-                    .map(|a| Ok::<AddressAttribute, rtnetlink::Error>(a))
-            ))
-            .try_flatten()
-            .try_collect::<Vec<AddressAttribute>>().await?
-            // Extract relevant addresses
-            .into_iter()
-            .flat_map(|a| if let AddressAttribute::Address(addr) = a {
-                Some(addr)
+    // Fetch link addresses
+    let addrs = handle.address().get()
+        .set_link_index_filter(link.header.index)
+        .execute()
+        // Extract attributes
+        .try_filter_map(|a| future::ready(
+            if a.header.family == AddressFamily::Inet {
+                Ok(Some(a.attributes))
             } else {
-                None
+                Ok(None)
             })
-            .collect::<Vec<IpAddr>>();
+        )
+        .map_ok(|attrs| stream::iter(
+            attrs.into_iter()
+                .map(|a| Ok::<AddressAttribute, rtnetlink::Error>(a))
+        ))
+        .try_flatten()
+        .try_collect::<Vec<AddressAttribute>>().await?
+        // Extract relevant addresses
+        .into_iter()
+        .flat_map(|a| if let AddressAttribute::Address(addr) = a {
+            Some(addr)
+        } else {
+            None
+        })
+        .collect::<Vec<IpAddr>>();
 
-        if addrs.len() == 0 {
-            bail!("No IPv4 address found for interface {ifname}")
-        }
-        if addrs.len() > 1 {
-            // Probably not possible under Linux? Check anyway.
-            bail!("Multiple IPv4 addresses found on for interface {ifname}")
-        }
-        for addr in addrs {
-            println!("Attr: {addr:#?}");
-        }
+    if addrs.len() == 0 {
+        bail!("No IPv4 address found for interface {ifname}")
     }
-
-    Ok(Ipv4Addr::new(1,1,1,1))
+    if addrs.len() > 1 {
+        // Not possible under Linux? Check anyway.
+        bail!("Multiple IPv4 addresses found on for interface {ifname}")
+    }
+    if let IpAddr::V4(ipaddr) = addrs[0] {
+        Ok(ipaddr)
+    } else {
+        bail!("Found non-IPv4 address on {ifname}; this is an internal logic error")
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::{future, net::IpAddr};
-
     use super::*;
-    use rtnetlink::{new_connection, packet_route::{address::{AddressAttribute}, AddressFamily}};
+    use tokio::fs::read_to_string;
     use tracing_test::traced_test;
-    use futures::{stream::{self, TryStreamExt}};
 
     #[tokio::test]
     #[traced_test]
     async fn test_fetch_addrs() -> Result<()> {
+        // Hack: parse an interface address out of kernel routes
+        let ifname = read_to_string("/proc/net/route").await?
+            .lines()
+            .skip(1) // header
+            .take(1)
+            .collect::<String>()
+            .split_whitespace()
+            .take(1)
+            .collect::<String>();
 
-        assert!(false);
+        let _ip = get_if_addr(&ifname).await?;
+
         Ok(())
     }
 
