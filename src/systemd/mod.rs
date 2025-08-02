d4c46c6422e36a7ca24c2171e1e6cf1f4d047ca1
Steve <tarkasteve@gmail.com>
2025-08-02 13:53:38 +1000

    Interim checkin; start of dbus integration.

--- src/systemd/mod.rs
+++ src/systemd/mod.rs
@@ -1,4 +1,24 @@
 
 mod linux;
-//use tracing::{error, info, warn};
+mod network1;
 
+use anyhow::{bail, Context, Result};
+use tracing::{error, info, warn};
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use macro_rules_attribute::apply;
+    use smol_macros::test;
+    use tracing_test::traced_test;
+    use zbus::Connection;
+
+    #[apply(test!)]
+    #[traced_test]
+    async fn test_zbus_connect() -> Result<()> {
+        let conn = Connection::system().await?;
+        Ok(())
+    }
+
+}
