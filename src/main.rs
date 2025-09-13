4c015668b5ad7faa4f572bd415e9b4cadd4bdafe
Steve <tarkasteve@gmail.com>
2025-09-13 16:50:14 +1000

    HTTP client and Gandi code moved to new `dns-edit` project.

--- src/main.rs
+++ src/main.rs
@@ -15,15 +15,15 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 mod config;
-mod gandi;
-mod http;
 mod netlink;
 
 use std::{str::FromStr, time::Duration};
 
-use anyhow::Result;
+use anyhow::{bail, Result};
+use dns_edit::DnsProvider;
+use dns_edit::gandi::{Auth, Gandi};
 use futures::stream::StreamExt;
-use tracing::{info, warn};
+use tracing::{error, info, warn};
 use tracing_subscriber::{EnvFilter, filter::LevelFilter};
 
 use crate::netlink::ChangeType;
@@ -46,10 +46,30 @@ fn init_logging(level: &Option<String>) -> Result<()> {
     Ok(())
 }
 
+fn get_auth() -> Result<Auth> {
+   let config = config::get_config()?;
+   let auth = if let Some(key) = &config.gandi_api_key {
+       Auth::ApiKey(key.clone())
+    } else if let Some(key) = &config.gandi_pat_key {
+       Auth::PatKey(key.clone())
+    } else {
+        error!("No Gandi key set");
+        bail!("No Gandi key set");
+    };
+    Ok(auth)
+}
+
 fn main() -> Result<()> {
     let config = config::get_config()?;
     init_logging(&config.log_level)?;
     info!("Starting...");
+
+    let dns_conf = dns_edit::Config {
+        domain: config.domain.clone(),
+        dry_run: config.dry_run.unwrap_or(false),
+    };
+
+    let gandi = Gandi::new(dns_conf, get_auth()?);
 
     smol::block_on(async {
         info!("Waiting for {} to come up...", config.iface);
@@ -64,11 +84,11 @@ fn main() -> Result<()> {
             smol::Timer::after(Duration::from_secs(10)).await;
         };
 
-        let mut upstream = gandi::get_host_ipv4(&config.domain, &config.host).await?;
+        let mut upstream = gandi.get_v4_record(&config.host).await?;
 
         if Some(local) != upstream {
             info!("DNS record out of date; updating");
-            gandi::set_host_ipv4(&config.domain, &config.host, &local).await?;
+            gandi.set_v4_record(&config.host, &local).await?;
         }
 
         info!("Starting monitoring stream");
@@ -85,7 +105,7 @@ fn main() -> Result<()> {
                     }
 
                     info!("Setting DNS record");
-                    gandi::set_host_ipv4(&config.domain, &config.host, &ip).await?;
+                    gandi.set_v4_record(&config.host, &ip).await?;
                     info!("DNS Set");
                     upstream = Some(ip);
                 }
